a89642a46d151c4957cf60a138e25e4300cbb788
GabrielVitorSalesMachado <gabrielvitorsales@gmail.com>
2021-05-11 12:29:34 -0300

    Put functions in a folder named functions and add ft_atoi.c

---
+++ functions/ft_atoi.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_atoi.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gvitor-s <gvitor-s@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/05/11 12:16:45 by gvitor-s          #+#    #+#             */
+/*   Updated: 2021/05/11 12:27:54 by gvitor-s         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	ft_atoi(char *str)
+{
+	int	counter_str;
+	int	minus_signal;
+	int	result;
+
+	minus_signal = 0;
+	counter_str = 0;
+	if (str[counter_str] == '-')
+		minus_signal = 1;
+	result = 0;
+	while (str[counter_str])
+	{
+		result = result * 10 + (str[counter_str] - '0');
+		counter_str++;
+	}
+	if (minus_signal)
+		return (result * (-1));
+	return (result);
+}
---
+++ functions/ft_strlcpy.c
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcpy.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gvitor-s <gvitor-s@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/05/10 14:39:45 by gvitor-s          #+#    #+#             */
+/*   Updated: 2021/05/10 15:59:44 by gvitor-s         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	ft_strlcpy(char *dst, const char *src, int size)
+{
+	int	counter_i;
+	int	counter_j;
+
+	counter_i = 0;
+	counter_j = 0;
+	while (size-- > 0 && src[counter_j])
+		dst[counter_i++] = src[counter_j++];
+	return (counter_i);
+}
