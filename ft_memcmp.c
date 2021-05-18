9d7fd9400326537a446d83173a0b5216b90fa1a0
gabrielvitorsales <gabrielvitorsales@gmail.com>
2021-05-18 17:36:55 -0300

    fix: compare the first n bytes now

--- ft_memcmp.c
+++ ft_memcmp.c
@@ -6,7 +6,7 @@
 /*   By: gvitor-s <gvitor-s@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/17 16:38:59 by gvitor-s          #+#    #+#             */
-/*   Updated: 2021/05/18 14:16:01 by mmassaak         ###   ########.fr       */
+/*   Updated: 2021/05/18 17:34:40 by gvitor-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,8 +18,9 @@ int	ft_memcmp(const void *s1, const void *s2, size_t n)
 
 	c_s1 = ((unsigned char *)s1);
 	c_s2 = ((unsigned char *)s2);
-	while (*c_s1 == *c_s2 && *c_s1 && *c_s2)
+	while (*c_s1 == *c_s2 && *c_s1 && *c_s2 && n > 0)
 	{
+		n--;
 		c_s1++;
 		c_s2++;
 	}
