eb5cbe7ef32d5196be041fe43c3857f21174ab4e
gabrielvitorsales <gabrielvitorsales@gmail.com>
2021-05-18 14:55:18 -0300

    Remove folder functions and change parameter's type int to size_t

---
+++ ft_strncmp.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strncmp.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gvitor-s <gvitor-s@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/05/10 17:10:17 by gvitor-s          #+#    #+#             */
+/*   Updated: 2021/05/18 14:19:10 by mmassaak         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+int	ft_strncmp(char *s1, char *s2, size_t size_cmp)
+{
+	int	counter_s1;
+	int	counter_s2;
+
+	counter_s1 = 0;
+	counter_s2 = 0;
+	while (s1[counter_s1] == s2[counter_s2] && size_cmp > 0)
+	{
+		counter_s1++;
+		counter_s2++;
+		size_cmp--;
+	}
+	return ((unsigned char)s1[counter_s1] - (unsigned char)s2[counter_s2]);
+}
