9315b4f0dff840b26a5da4b1cca6333cc5592661
gabrielvitorsales <gabrielvitorsales@gmail.com>
2021-05-19 16:14:56 -0300

    fix: add NULL terminating character

--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -6,19 +6,24 @@
 /*   By: gvitor-s <gvitor-s@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/18 18:39:30 by gvitor-s          #+#    #+#             */
-/*   Updated: 2021/05/18 18:39:32 by gvitor-s         ###   ########.fr       */
+/*   Updated: 2021/05/19 15:51:43 by gvitor-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h" 
 size_t	ft_strlcpy(char *dst, const char *src, size_t size)
 {
-	size_t	counter_i;
 	size_t	counter_j;
 
-	counter_i = 0;
 	counter_j = 0;
+	if (size == 0)
+	{
+		while (src[counter_j])
+			counter_j++;
+		return (counter_j);
+	}
 	while (size-- > 0 && src[counter_j])
-		dst[counter_i++] = src[counter_j++];
-	return (counter_i);
+		dst[counter_j++] = src[counter_j++];
+	dst[counter_j] = '\0';
+	return (counter_j);
 }
