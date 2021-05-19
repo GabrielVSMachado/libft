59f6a52d13450d04cf9806044c19f9a9bc59f894
gabrielvitorsales <gabrielvitorsales@gmail.com>
2021-05-19 16:09:43 -0300

    add: ft_calloc

---
+++ ft_calloc.c
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_calloc.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gvitor-s <gvitor-s@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/05/19 14:38:00 by gvitor-s          #+#    #+#             */
+/*   Updated: 2021/05/19 15:29:18 by gvitor-s         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+void	*ft_calloc(size_t ntype, size_t size)
+{
+	void	*r;
+
+	if (ntype == 0 || size == 0)
+		return (NULL);
+	r = malloc(ntype * size);
+	return (r);
+}
