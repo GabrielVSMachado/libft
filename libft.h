7fc3afc6fbf1e55562212a3947609baf843b7653
gabrielvitorsales <gabrielvitorsales@gmail.com>
2021-05-20 13:21:46 -0300

    feat: add ft_bzero prototype

--- libft.h
+++ libft.h
@@ -16,6 +16,7 @@
 # include <stdlib.h>
 # include <stddef.h>
 
+void	ft_bzero(void *s, size_t n);
 void	*ft_memset(void *s, int c, size_t n);
 void	*ft_memcpy(void *dest, const void *src, size_t n);
 void	*ft_memccpy(void *dest, const void *src, int c, size_t n);
