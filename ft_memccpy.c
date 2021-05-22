c8388dbc6cb2b11155a511047687f643b9e4b2a7
gabrielvitorsales <gabrielvitorsales@gmail.com>
2021-05-22 14:30:02 -0300

    fix: adjust conforming test

--- ft_memccpy.c
+++ ft_memccpy.c
@@ -11,22 +11,21 @@
 /* ************************************************************************** */
 
 #include "libft.h" 
-void	*ft_memccpy(void *restrict dst, const void *restrict src,
-		int c, size_t buffer_size)
+void	*ft_memccpy(void *restrict dst, const void *restrict src, int c, size_t n)
 {
-	size_t	counter_dst;
-	size_t	counter_src;
-
-	counter_dst = 0;
-	counter_src = 0;
-	while (buffer_size > counter_dst)
+	while (n)
 	{
-		if (((char *)src)[counter_src] == (char)c)
-			break ;
-		else
-			((char *)dst)[counter_dst] = ((char *)src)[counter_src];
-		counter_dst++;
-		counter_src++;
+		*((char *)dst) = *((char *)src);
+		if (*((char *)src) == c)
+		{
+			dst++;
+			src++;
+			*((char *)dst) = *((char *)src);
+			return (dst);
+		}
+		dst++;
+		src++;
+		n--;
 	}
-	return (dst);
+	return (NULL);
 }
