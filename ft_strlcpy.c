6263b6f9a5ec2476341135fea561feac6eb540a8
Gabriel Vitor sales machado <gvitor-s@c1r5p2.42sp.org.br>
2021-05-23 16:09:48 -0300

    fix: size != 0

--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -6,7 +6,7 @@
 /*   By: gvitor-s <gvitor-s@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/18 18:39:30 by gvitor-s          #+#    #+#             */
-/*   Updated: 2021/05/23 12:53:59 by gvitor-s         ###   ########.fr       */
+/*   Updated: 2021/05/23 16:07:34 by gvitor-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,7 @@ size_t	ft_strlcpy(char *dst, const char *src, size_t size)
 	if (size == 0)
 		return (len_src);
 	counter_dst = 0;
-	while (--size > 0 && *src)
+	while (--size != 0 && *src)
 		*(dst + counter_dst++) = *(src++);
 	*(dst + counter_dst) = '\0';
 	return (len_src);
