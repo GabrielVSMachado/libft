23baa9e227e81cea41be121188a6e71ac8d8a5be
Gabriel Vitor sales machado <gvitor-s@c1r5p2.42sp.org.br>
2021-05-23 11:27:27 -0300

    feat: add  return to size_cmp == 0

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: gvitor-s <gvitor-s@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/18 18:40:07 by gvitor-s          #+#    #+#             */
-/*   Updated: 2021/05/18 18:59:07 by gvitor-s         ###   ########.fr       */
+/*   Updated: 2021/05/23 11:26:25 by gvitor-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,13 +16,14 @@ int	ft_strncmp(const char *s1, const char *s2, size_t size_cmp)
 	int	counter_s1;
 	int	counter_s2;
 
+	if (size_cmp == 0)
+		return (0);
 	counter_s1 = 0;
 	counter_s2 = 0;
-	while (s1[counter_s1] == s2[counter_s2] && size_cmp > 0)
+	while (s1[counter_s1] == s2[counter_s2] && --size_cmp > 0)
 	{
 		counter_s1++;
 		counter_s2++;
-		size_cmp--;
 	}
 	return ((unsigned char)s1[counter_s1] - (unsigned char)s2[counter_s2]);
 }
