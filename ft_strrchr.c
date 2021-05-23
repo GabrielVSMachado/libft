946a65281381fe78f7bdf93e2a103597d3f3b346
Gabriel Vitor sales machado <gvitor-s@c1r5p2.42sp.org.br>
2021-05-23 11:21:03 -0300

    fix: make return to c == 0 and when c is not found

--- ft_strrchr.c
+++ ft_strrchr.c
@@ -6,19 +6,27 @@
 /*   By: gvitor-s <gvitor-s@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/19 13:43:37 by gvitor-s          #+#    #+#             */
-/*   Updated: 2021/05/19 13:51:48 by gvitor-s         ###   ########.fr       */
+/*   Updated: 2021/05/23 10:53:28 by gvitor-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include "libft.h"
 
 char	*ft_strrchr(const char *str, int c)
 {
 	int	counter;
 
 	counter = 0;
+	if ((unsigned char)c == '\0')
+		return ((char *)(str + ft_strlen(str)));
 	while (str[counter])
 		counter++;
 	counter--;
-	while (str[counter] && str[counter] != c)
+	while (counter >= 0)
+	{
+		if (str[counter] == (unsigned char)c)
+			return ((char *)(str + counter));
 		counter--;
-	return ((char *)&str[counter]);
+	}
+	return (NULL);
 }
