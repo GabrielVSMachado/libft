52e605c596742741a5d3a07b6790df06ac601f52
gabrielvitorsales <gabrielvitorsales@gmail.com>
2021-05-23 08:14:21 -0300

    fix: cast to unsigned char and return when c is 0

--- ft_memchr.c
+++ ft_memchr.c
@@ -20,11 +20,9 @@ void	*ft_memchr(const void *str, int c, size_t n)
 	c_str = (unsigned char *)str;
 	while (n > counter)
 	{
-		if (c_str[counter] == c)
-			break ;
+		if (c_str[counter] == (unsigned char)c)
+			return ((void *)str + counter);
 		counter++;
 	}
-	if (!c_str[counter])
-		return (NULL);
-	return ((void *)str + counter);
+	return (NULL);
 }
