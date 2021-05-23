c4ee8d2e15de7ec332ee22bab273973521479b36
Gabriel Vitor sales machado <gvitor-s@c1r5p2.42sp.org.br>
2021-05-23 09:37:46 -0300

    fix: resolve conflit btween return  c == \0 and not found c

--- ft_strchr.c
+++ ft_strchr.c
@@ -6,13 +6,21 @@
 /*   By: gvitor-s <gvitor-s@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/19 12:45:48 by gvitor-s          #+#    #+#             */
-/*   Updated: 2021/05/19 13:32:08 by gvitor-s         ###   ########.fr       */
+/*   Updated: 2021/05/23 09:28:14 by gvitor-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
+
 char	*ft_strchr(const char *str, int c)
 {
-	while (*str && *str != (char) c)
+	if (c == '\0')
+		return ((char *)(str + ft_strlen(str)));
+	while (*str)
+	{
+		if (*str == (unsigned char) c)
+			return ((char *)str);
 		str++;
-	return ((char *)str);
+	}
+	return (NULL);
 }
