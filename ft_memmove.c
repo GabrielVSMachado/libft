c692a4743a2a398a06ca8272508feb12ad0af8b2
Gabriel Vitor sales machado <gvitor-s@c1r1p4.42sp.org.br>
2021-05-24 08:17:11 -0300

    fix: copy to memory  is not NULL terminating(--n)

--- ft_memmove.c
+++ ft_memmove.c
@@ -6,7 +6,7 @@
 /*   By: gvitor-s <gvitor-s@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/18 17:43:30 by gvitor-s          #+#    #+#             */
-/*   Updated: 2021/05/23 15:40:21 by gvitor-s         ###   ########.fr       */
+/*   Updated: 2021/05/24 08:14:51 by gvitor-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,18 +15,13 @@ void	*ft_memmove(void *dst, const void *src, size_t n)
 {
 	char	*c_src;
 	char	*c_dst;
-	size_t	counter_dst;
 
 	c_src = (char *)src;
 	c_dst = (char *)dst;
-	counter_dst = 0;
 	if (src > dst)
 	{
-		while (counter_dst < n)
-		{
-			c_dst[counter_dst] = c_src[counter_dst];
-			counter_dst++;
-		}
+		while (n-- != 0)
+			*(c_dst++) = *(c_src++);
 	}
 	else
 	{
