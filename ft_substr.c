3cf5fe6259f7b01067952db5eff50d514a81704c
Gabriel Vitor sales machado <gvitor-s@c1r1p4.42sp.org.br>
2021-05-24 11:40:42 -0300

    feat: alloc memory needed

--- ft_substr.c
+++ ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: gvitor-s <gvitor-s@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/19 16:18:29 by gvitor-s          #+#    #+#             */
-/*   Updated: 2021/05/19 19:52:45 by gvitor-s         ###   ########.fr       */
+/*   Updated: 2021/05/24 11:35:16 by gvitor-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,13 +15,29 @@ char	*ft_substr(char const *str, unsigned int start, size_t len)
 {
 	char	*substr;
 	size_t	counter;
+	size_t	len_alloc;
+	size_t	len_str;
 
-	substr = (char *)malloc(sizeof(char) * len);
+	/* Alocate space conforme the needing */
+	len_str = ft_strlen(str);
+	if (len_str < (size_t)start)
+		len_alloc = 1;
+	else if (len_str - (size_t)start > len)
+		len_alloc = len;
+	else
+		len_alloc = len_str - (size_t)start;
+
+	substr = (char *)malloc(sizeof(char) * (len_alloc + 1));
+	/* check malloc */
 	if (substr == NULL)
 		return (NULL);
-	counter = -1;
-	while (++counter < len)
+	/* Copy substr */
+	counter = 0;
+	while (counter < len && (len_str > (size_t)start))
+	{
 		substr[counter] = str[start + counter];
+		counter++;
+	}
 	substr[counter] = '\0';
 	return (substr);
 }
