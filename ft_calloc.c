2df973cf09f6a9914ba48ebd411cff7e24055d8b
Gabriel Vitor sales machado <gvitor-s@c1r1p1.42sp.org.br>
2021-05-27 15:23:11 -0300

    fix: work if size == 0

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,7 +6,7 @@
 /*   By: gvitor-s <gvitor-s@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/19 14:38:00 by gvitor-s          #+#    #+#             */
-/*   Updated: 2021/05/27 14:54:56 by gvitor-s         ###   ########.fr       */
+/*   Updated: 2021/05/27 15:09:29 by gvitor-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,8 +15,6 @@ void	*ft_calloc(size_t ntype, size_t size)
 {
 	void	*r;
 
-	if (ntype == 0 || size == 0)
-		return (NULL);
 	r = malloc(ntype * size);
 	if (!r)
 		return (NULL);
