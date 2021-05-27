71a63eaa704cddb5461d9f07d69e7a783f8b7eba
Gabriel Vitor sales machado <gvitor-s@c1r1p1.42sp.org.br>
2021-05-27 15:25:46 -0300

    fix: when both are \0

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,24 +6,20 @@
 /*   By: gvitor-s <gvitor-s@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/18 18:40:07 by gvitor-s          #+#    #+#             */
-/*   Updated: 2021/05/24 09:10:53 by gvitor-s         ###   ########.fr       */
+/*   Updated: 2021/05/27 15:18:21 by gvitor-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 int	ft_strncmp(const char *s1, const char *s2, size_t size_cmp)
 {
-	int	counter_s1;
-	int	counter_s2;
 
 	if (size_cmp == 0)
 		return (0);
-	counter_s1 = 0;
-	counter_s2 = 0;
-	while (s1[counter_s1] == s2[counter_s2] && --size_cmp != 0)
+	while (*s1 == *s2 && --size_cmp != 0 && *s2 && *s1)
 	{
-		counter_s1++;
-		counter_s2++;
+		s1++;
+		s2++;
 	}
-	return ((unsigned char)s1[counter_s1] - (unsigned char)s2[counter_s2]);
+	return ((unsigned char)*s1 - (unsigned char)*s2);
 }
