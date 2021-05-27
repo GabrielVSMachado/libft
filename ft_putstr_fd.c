9f9a3df18b7ba77b04634f511c02d9c80de734f4
GabrielVSM <gabrielvitorsales@gmail.com>
2021-05-27 11:15:10 -0300

    feat: function ft_putstr_fd

---
+++ ft_putstr_fd.c
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putstr_fd.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gvitor-s <gvitor-s@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/05/27 11:09:03 by gvitor-s          #+#    #+#             */
+/*   Updated: 2021/05/27 11:12:38 by gvitor-s         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+void	ft_putstr_fd(char *s, int fd)
+{
+	size_t	counter;
+
+	counter = 0;
+	while (s[counter++])
+		ft_putchar_fd(s[counter], fd);
+}
