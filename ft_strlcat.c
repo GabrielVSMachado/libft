f16e295c16e8ff87ddfe3f535d513d78cbcae11b
GabrielVSM <gabrielvitorsales@gmail.com>
2021-05-28 15:13:06 +0000

    feat: add if conditions

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -6,26 +6,26 @@
 /*   By: gvitor-s <gvitor-s@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/18 18:39:10 by gvitor-s          #+#    #+#             */
-/*   Updated: 2021/05/28 12:47:50 by gvitor-s         ###   ########.fr       */
+/*   Updated: 2021/05/28 15:09:45 by gvitor-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+#include "libft.h"
 
-#include "libft.h" 
-size_t	ft_strlcat(char *dst, const char *src, size_t len_dst_final)
+size_t  ft_strlcat(char *dst, const char *src, size_t len_dst_final)
 {
-	int		counter;
-	char	*src_tmp;
-	int		len_dst;
+    int	     counter;
+    char    *src_tmp;
+    size_t     len_dst;
 
-	len_dst = ft_strlen(dst);
-	counter = 0;
-	while (--len_dst_final != 0 && dst[counter])
-		counter++;
-	if (len_dst_final == 0)
-		return (len_dst);
-	while (*src_tmp && len_dst_final-- != 1)
-		dst[counter++] = *(src_tmp++);
-	dst[counter] = '\0';
-	return ((src_tmp - src) + len_dst);
+    src_tmp = (char *)src;
+    len_dst = ft_strlen(dst);
+    counter = 0;
+    if (len_dst_final <= len_dst)
+        return (ft_strlen(src) + len_dst_final);
+	len_dst_final -= len_dst;
+    while (*src_tmp && len_dst_final-- != 1)
+        dst[len_dst + counter++] = *(src_tmp++);
+    dst[counter + len_dst] = '\0';
+    return ((src_tmp - src) + len_dst);
 
 }
