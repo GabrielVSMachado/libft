0cb69ad79c55cd9021053f604bb2d94a15e0f5f2
Gabriel Vitor sales machado <gvitor-s@c1r4p3.42sp.org.br>
2021-05-28 16:06:01 -0300

    feat: protect to NULL params

--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -6,7 +6,7 @@
 /*   By: gvitor-s <gvitor-s@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/18 18:39:30 by gvitor-s          #+#    #+#             */
-/*   Updated: 2021/05/23 16:07:34 by gvitor-s         ###   ########.fr       */
+/*   Updated: 2021/05/28 16:05:19 by gvitor-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,8 @@ size_t	ft_strlcpy(char *dst, const char *src, size_t size)
 	size_t	len_src;
 	size_t	counter_dst;
 
+	if (!src || !dst)
+		return (0);
 	len_src = ft_strlen(src);
 	if (size == 0)
 		return (len_src);
