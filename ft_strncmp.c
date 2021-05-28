80a7bc73a35e391df508ed34ac334c3fcd87a8df
Gabriel Vitor sales machado <gvitor-s@c1r4p3.42sp.org.br>
2021-05-28 15:27:35 -0300

    fix: adjust to  norminette

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,14 +6,13 @@
 /*   By: gvitor-s <gvitor-s@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/18 18:40:07 by gvitor-s          #+#    #+#             */
-/*   Updated: 2021/05/27 15:18:21 by gvitor-s         ###   ########.fr       */
+/*   Updated: 2021/05/28 15:26:56 by gvitor-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 int	ft_strncmp(const char *s1, const char *s2, size_t size_cmp)
 {
-
 	if (size_cmp == 0)
 		return (0);
 	while (*s1 == *s2 && --size_cmp != 0 && *s2 && *s1)
