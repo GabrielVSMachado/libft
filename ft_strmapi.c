da04da0bc2e7da8db9e56c113a3aff18f339ee2e
Gabriel Vitor sales machado <gvitor-s@c1r4p3.42sp.org.br>
2021-05-28 16:01:18 -0300

    fix: counter error

--- ft_strmapi.c
+++ ft_strmapi.c
@@ -6,7 +6,7 @@
 /*   By: gvitor-s <gvitor-s@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/26 16:29:37 by gvitor-s          #+#    #+#             */
-/*   Updated: 2021/05/26 20:09:51 by gvitor-s         ###   ########.fr       */
+/*   Updated: 2021/05/28 15:56:20 by gvitor-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,11 +17,13 @@ char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
 	char			*str_final;
 	unsigned int	counter;
 
-	counter = 0;
+	counter = -1;
+	if (s == NULL)
+		return (NULL);
 	str_final = (char *)malloc(sizeof(char) * (ft_strlen(s) + 1));
 	if (!str_final)
 		return (NULL);
-	while (s[counter++])
+	while (s[++counter])
 		*(str_final + counter) = (*f)(counter, s[counter]);
 	*(str_final + counter) = '\0';
 	return (str_final);
