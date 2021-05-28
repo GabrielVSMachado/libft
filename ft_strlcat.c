b5a72f97df2cd5184987318c1007525f38f5c9fd
GabrielVSM <gabrielvitorsales@gmail.com>
2021-05-28 12:48:22 +0000

    refactory: ft_strlcat

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -6,22 +6,26 @@
 /*   By: gvitor-s <gvitor-s@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/18 18:39:10 by gvitor-s          #+#    #+#             */
-/*   Updated: 2021/05/19 15:58:49 by gvitor-s         ###   ########.fr       */
+/*   Updated: 2021/05/28 12:47:50 by gvitor-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h" 
-size_t	ft_strlcat(char *dst, const char *src, size_t buffer_size)
+size_t	ft_strlcat(char *dst, const char *src, size_t len_dst_final)
 {
-	size_t	counter_i;
-	size_t	counter_j;
+	int		counter;
+	char	*src_tmp;
+	int		len_dst;
 
-	counter_i = 0;
-	counter_j = 0;
-	while (dst[counter_i])
-		counter_i++;
-	while (counter_i < buffer_size && src[counter_j])
-		dst[counter_i++] = src[counter_j++];
-	dst[counter_i] = '\0';
-	return (counter_i + counter_j);
+	len_dst = ft_strlen(dst);
+	counter = 0;
+	while (--len_dst_final != 0 && dst[counter])
+		counter++;
+	if (len_dst_final == 0)
+		return (len_dst);
+	while (*src_tmp && len_dst_final-- != 1)
+		dst[counter++] = *(src_tmp++);
+	dst[counter] = '\0';
+	return ((src_tmp - src) + len_dst);
+
 }
