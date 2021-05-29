80edd8c8b79ddba2541c3f9082e3c29afaabb059
GabrielVSM <gabrielvitorsales@gmail.com>
2021-05-29 09:47:42 +0000

    refactor: version iterative for test

--- ft_itoa.c
+++ ft_itoa.c
@@ -6,45 +6,40 @@
 /*   By: gvitor-s <gvitor-s@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/24 22:20:06 by gvitor-s          #+#    #+#             */
-/*   Updated: 2021/05/28 16:09:13 by gvitor-s         ###   ########.fr       */
+/*   Updated: 2021/05/29 09:40:08 by gvitor-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-#include <limits.h>
-static size_t	len_n(unsigned int n);
-static char		convert_str_n(char *n_str, unsigned int n, unsigned int i);
+#include <stdio.h>
+static size_t	len_n(int n);
+static void		convert_n_str(char *p_str, int n, size_t len);
 
 char	*ft_itoa(int n)
 {
-	char			*n_str;
-	unsigned int	n_tmp;
+	int		n_tmp;
+	char	*n_str;
+	char	*p_str;
+	size_t	len;
 
-	if (n < 0)
-	{
-		n_tmp = (size_t)(-1) * n;
-		n_str = (char *)malloc(sizeof(char) * (len_n(n_tmp) + 2));
-		n_str[0] = '-';
-		convert_str_n(n_str, n_tmp, 1);
-	}
-	else
-	{
-		n_tmp = n;
-		n_str = (char *)malloc(sizeof(char) * (len_n(n_tmp) + 1));
-		convert_str_n(n_str, n_tmp, 0);
-	}
-	if (n_str == NULL)
+	n_tmp = n;
+	len = len_n(n_tmp);
+	n_str = (char *)malloc(sizeof(char) * (len + 1));
+	if (!n_str)
 		return (NULL);
+	p_str = n_str;
+	convert_n_str(p_str, n, len);
 	return (n_str);
 }
 
-static size_t	len_n(unsigned int n_tmp)
+static size_t	len_n(int n_tmp)
 {
 	size_t	len;
 
-	len = 0;
-	if (n_tmp == (unsigned int)INT_MAX)
-		return (11);
+	if (n_tmp < 0)
+		len = 1;
+	else
+		len = 0;
 	while (n_tmp)
 	{
 		n_tmp /= 10;
@@ -53,9 +48,30 @@ static size_t	len_n(unsigned int n_tmp)
 	return (len);
 }
 
-static char	convert_str_n(char *n_str, unsigned int n, unsigned int i)
+static void	convert_n_str(char *p_str, int n, size_t len)
 {
-	if (n < 10)
-		return (n_str[i++] = n % 10);
-	return (convert_str_n(n_str, n / 10, i));
+	size_t	tmp;
+	size_t	power_of_10;
+
+	if (n < 0)
+	{
+		tmp = (size_t)(-1) * n;
+		len--;
+		*p_str = '-';
+		p_str++;
+	}
+	else
+		tmp = n;
+	power_of_10 = 1;
+	while (len != 1)
+	{
+		power_of_10 *= 10;
+		len--;
+	}
+	while (power_of_10)
+	{
+		*(p_str++) = ((tmp / power_of_10) % 10) + '0';
+		power_of_10 /= 10;
+	}
+	*p_str = '\0';
 }
