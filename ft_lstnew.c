b5af2788b774751eaf8acb50fce771b2d5fe05ef
GabrielVSM <gabrielvitorsales@gmail.com>
2021-05-29 14:35:36 +0000

    feat: ft_lstnew

---
+++ ft_lstnew.c
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstnew.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gvitor-s <gvitor-s@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/05/29 11:53:04 by gvitor-s          #+#    #+#             */
+/*   Updated: 2021/05/29 12:11:47 by gvitor-s         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+t_list	*ft_lstnew(void *content)
+{
+	t_list	*created;
+
+	content = malloc(sizeof(content));
+	created->content = content;
+	created->next = NULL;
+	return (created);
+}
