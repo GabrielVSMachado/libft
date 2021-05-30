f0b00a116df0d3d0a4fd434d88d015ee6784f421
Gabriel Vitor sales machado <gvitor-s@c1r2p3.42sp.org.br>
2021-05-30 15:26:13 -0300

    fix: protect the malloc function

--- ft_lstnew.c
+++ ft_lstnew.c
@@ -6,7 +6,7 @@
 /*   By: gvitor-s <gvitor-s@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/29 11:53:04 by gvitor-s          #+#    #+#             */
-/*   Updated: 2021/05/29 15:50:39 by gvitor-s         ###   ########.fr       */
+/*   Updated: 2021/05/30 14:56:15 by gvitor-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,8 @@ t_list	*ft_lstnew(void *content)
 	t_list	*created;
 
 	created = (t_list *)malloc(sizeof(t_list));
+	if (!created)
+		return (NULL);
 	created->content = content;
 	created->next = NULL;
 	return (created);
