68faf1c83a89a287ef7f9b3a26b3149e74debe25
Gabriel Vitor sales machado <gvitor-s@c1r2p3.42sp.org.br>
2021-05-30 15:25:26 -0300

    fix: stop at the last struct not the NULL

--- ft_lstlast.c
+++ ft_lstlast.c
@@ -6,7 +6,7 @@
 /*   By: gvitor-s <gvitor-s@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/29 13:38:10 by gvitor-s          #+#    #+#             */
-/*   Updated: 2021/05/29 14:31:23 by gvitor-s         ###   ########.fr       */
+/*   Updated: 2021/05/30 14:58:48 by gvitor-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,9 +17,12 @@ t_list	*ft_lstlast(t_list *lst)
 	t_list	*p_tmp;
 	int		len;
 
-	len = ft_lstsize(lst);
+	len = ft_lstsize(lst) - 1;
 	p_tmp = lst;
-	while (--len != 0)
+	while (len > 0)
+	{
 		p_tmp = p_tmp->next;
+		len--;
+	}
 	return (p_tmp);
 }
