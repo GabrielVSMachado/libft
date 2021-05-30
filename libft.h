28192e24fbe5fc71d037cd1daf8beace2d485173
GabrielVSM <gabrielvitorsales@gmail.com>
2021-05-30 14:04:50 +0000

    feat: add function ft_lstiter

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: gvitor-s <gvitor-s@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/18 17:08:19 by gvitor-s          #+#    #+#             */
-/*   Updated: 2021/05/30 13:41:03 by gvitor-s         ###   ########.fr       */
+/*   Updated: 2021/05/30 13:58:57 by gvitor-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -64,4 +64,5 @@ t_list	*ft_lstlast(t_list *lst);
 void	ft_lstadd_back(t_list **lst, t_list *new);
 void	ft_lstdelone(t_list *lst, void (*del)(void *));
 void	ft_lstclear(t_list **lst, void (*del)(void *));
+void	ft_lstiter(t_list *lst, void (*f)(void *));
 #endif
