50d443279d4c84ec8f711d274ff2f092c4a339ec
GabrielVSM <gabrielvitorsales@gmail.com>
2021-05-30 21:52:29 +0000

    fix: loop of number_of_pointers

--- ft_split.c
+++ ft_split.c
@@ -6,12 +6,12 @@
 /*   By: gvitor-s <gvitor-s@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/30 20:07:06 by gvitor-s          #+#    #+#             */
-/*   Updated: 2021/05/30 21:13:00 by gvitor-s         ###   ########.fr       */
+/*   Updated: 2021/05/30 21:51:18 by gvitor-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-static int	n_pointers(char *str_tmp , char c);
+static int	n_pointers(char *str_tmp , char c ,char *set);
 
 char	**ft_split(const char *s, char c)
 {
@@ -24,8 +24,9 @@ char	**ft_split(const char *s, char c)
 
 	set[0] = c;
 	set[1] = '\0';
+	str_tmp = (char *)s;
+	str = (char **)malloc(sizeof(char *) * (n_pointers(str_tmp, c, set) + 1));
 	str_tmp = ft_strtrim(s, set);
-	str = (char **)malloc(sizeof(char *) * (n_pointers(str_tmp, c) + 1));
 	counter = 0;
 	while (str_tmp)
 	{
@@ -41,21 +42,18 @@ char	**ft_split(const char *s, char c)
 	return (str);
 }
 
-static int	n_pointers(char *str_tmp, char c)
+static int	n_pointers(char *str_tmp, char c, char *set)
 {
 	int	number_of_pointer;
-	int	counter;
 	int	len;
 	char	*next_char_c;
 
 	number_of_pointer = 0;
-	counter = 0;
-	while (str_tmp[counter])
+	while (str_tmp)
 	{
-		while (str_tmp[counter] == c && str_tmp[counter + 1] == c)
-			counter++;
-		next_char_c = ft_strchr(&str_tmp[counter], c);
-		len = next_char_c - &str_tmp[counter];
+		str_tmp = ft_strtrim(str_tmp, set);
+		next_char_c = ft_strchr(str_tmp, c);
+		len = next_char_c - str_tmp;
 		if (!next_char_c)
 		{
 			number_of_pointer++;
@@ -64,9 +62,10 @@ static int	n_pointers(char *str_tmp, char c)
 		else if (len > 0) 
 		{
 			number_of_pointer++;
-			counter += len + 1;
+			str_tmp += len + 1;
 			continue ;
 		}
+		
 	}
 	return (number_of_pointer);
 }
