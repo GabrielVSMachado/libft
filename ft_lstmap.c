dbcc5c2ef878721de1f32ac15c96f2e2320c78cc
GabrielVSM <gabrielvitorsales@gmail.com>
2021-05-31 22:04:42 +0000

    fix: remove pointer useless and add function ft_lstadd_back

--- ft_lstmap.c
+++ ft_lstmap.c
@@ -6,7 +6,7 @@
 /*   By: gvitor-s <gvitor-s@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/31 18:42:35 by gvitor-s          #+#    #+#             */
-/*   Updated: 2021/05/31 21:26:06 by gvitor-s         ###   ########.fr       */
+/*   Updated: 2021/05/31 22:03:52 by gvitor-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,21 +15,18 @@
 t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
 	t_list	*new;
-	t_list	*p_tmp;
 
-	while (lst)
+	new = NULL;
+	while (lst != NULL)
 	{
-		 p_tmp = ft_lstnew(f(lst->content));
-		 if (!p_tmp)
+		 ft_lstadd_back(&new, ft_lstnew(f(lst->content)));
+		 if (!new)
 		 {
 			 ft_lstclear(&new, del);
-			 free(p_tmp);
+			 free(new);
 			 return (NULL);
 		 }
 		 lst = lst->next;
-		 p_tmp = p_tmp->next;
 	}
-	new = p_tmp;
-	free(p_tmp);
 	return (new);
 }
