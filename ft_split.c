ab9162c5f4000c576b44db07886816f7e26d4e4f
Gabriel Vitor sales machado <gvitor-s@c1r1p4.42sp.org.br>
2021-05-31 00:02:32 -0300

    feat: add function check to protect the allocations

--- ft_split.c
+++ ft_split.c
@@ -6,13 +6,13 @@
 /*   By: gvitor-s <gvitor-s@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/30 20:07:06 by gvitor-s          #+#    #+#             */
-/*   Updated: 2021/05/30 23:28:00 by gvitor-s         ###   ########.fr       */
+/*   Updated: 2021/05/31 00:00:01 by gvitor-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 static int	n_pointers(char *str_tmp , char c ,char *set);
-static int	check(char **s);
+static int	check(char **s, int num_p);
 
 char	**ft_split(const char *s, char c)
 {
@@ -25,8 +25,10 @@ char	**ft_split(const char *s, char c)
 	set[0] = c;
 	set[1] = '\0';
 	str_tmp = ft_strtrim(s, set);
+	if (!str_tmp)
+		return (NULL);
 	str = (char **)malloc(sizeof(char *) * (n_pointers(str_tmp, c, set) + 1));
-	if (!check(str))
+	if (!str)
 		return (NULL);
 	num_p = 0;
 	while (*str_tmp)
@@ -35,7 +37,7 @@ char	**ft_split(const char *s, char c)
 		if (next_char_c - str_tmp > 0)
 		{
 			*(str + num_p) = ft_substr(str_tmp, 0, next_char_c - str_tmp);
-			if (!check(str + num_p))
+			if (check(str, num_p))
 				return (NULL);
 			num_p++;
 			str_tmp = next_char_c;
@@ -43,13 +45,13 @@ char	**ft_split(const char *s, char c)
 		else if (!next_char_c)
 		{
 			*(str + num_p) = ft_substr(str_tmp, 0, ft_strlen(str_tmp));
-			if (!check(str + num_p))
+			if (check(str, num_p))
 				return (NULL);
 			num_p++;
 			break ;
 		}
 		str_tmp = ft_strtrim(str_tmp, set);
-		if (!check((char **)str_tmp))
+		if (!str_tmp)
 			return (NULL);
 	}
 	*(str + num_p) = NULL;
@@ -66,7 +68,7 @@ static int	n_pointers(char *str_tmp, char c, char *set)
 	while (*str_tmp)
 	{
 		str_tmp = ft_strtrim(str_tmp, set);
-		if (!check((char **)str_tmp))
+		if (!str_tmp)
 			return (0);
 		next_char_c = ft_strchr(str_tmp, c);
 		len = next_char_c - str_tmp;
@@ -86,13 +88,20 @@ static int	n_pointers(char *str_tmp, char c, char *set)
 	return (number_of_pointer);
 }
 
-static int	check(char **s)
+static int	check(char **s , int num_p)
 {
-	if (!s)
+	int	i;
+
+	i = 0;
+	if (!*(s + num_p))
 	{
-		while (*s)
-			free(*s++);
-		return (0);
+		while (i < num_p)
+		{
+			free(s[i]);
+			i++;
+		}
+		free(s);
+		return (1);
 	}
-	return (1);
+	return (0);
 }
