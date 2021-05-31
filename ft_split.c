a8963c5ff7228a4f010f10f59ff0a89c5d5ceacf
GabrielVSM <gabrielvitorsales@gmail.com>
2021-05-31 17:06:35 +0000

    fix: memory leaks

--- ft_split.c
+++ ft_split.c
@@ -6,7 +6,7 @@
 /*   By: gvitor-s <gvitor-s@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/31 09:07:56 by gvitor-s          #+#    #+#             */
-/*   Updated: 2021/05/31 15:51:05 by gvitor-s         ###   ########.fr       */
+/*   Updated: 2021/05/31 16:52:51 by gvitor-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,22 +19,23 @@ char	**ft_split(const char *s, char c)
 {
 	char	*p_tmp;
 	char	**str;
-	int		num_p;
 	char	set[2];
+	char	*pp;
 
 	if (!s)
 		return (NULL);
 	set[0] = c;
 	set[1] = 0;
-	str = NULL;
 	p_tmp = ft_strtrim(s, set);
-	num_p = n_pointers(p_tmp, c);
+	str = (char **)ft_calloc(sizeof(char *), n_pointers(p_tmp, c) + 1);
+	pp = p_tmp;
 	while (*p_tmp)
 	{
 		str = alloc_str(str, &p_tmp, c);
 		while (*p_tmp == c && c != 0)
 			p_tmp++;
 	}
+	free(pp);
 	return (str);
 }
 
@@ -46,13 +47,8 @@ static char	**alloc_str(char **str, char **p_tmp, int c)
 	len = 0;
 	while ((p_tmp[0][len]) != c && p_tmp[0][len] != '\0')
 		len++;
-	if (!str)
-	{
-		str = (char **)ft_calloc(sizeof(char *), n_pointers(p_tmp[0], c) + 1);
-		if (!str)
-			return (NULL);
+	if (!str[0])
 		num_p = 0;
-	}
 	*(str + (num_p)) = ft_substr(p_tmp[0], 0, len);
 	if (check(str, num_p))
 		return (NULL);
