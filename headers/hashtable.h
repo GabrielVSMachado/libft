4a002cef20aa37ce899d1b0051ba2ed714b2aca7
Gabriel <gabrielvitorsales@gmail.com>
2021-12-16 23:07:47 -0300

    feat: move headers to folder headers

---
+++ headers/hashtable.h
@@ -0,0 +1,19 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   hashtable.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gvitor-s <gvitor-s@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/12/16 21:10:40 by gvitor-s          #+#    #+#             */
+/*   Updated: 2021/12/16 21:10:40 by gvitor-s         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef HASHTABLE_H
+# define HASHTABLE_H
+
+# define TABLE_SIZE 10;
+
+int	hash_generate(const void *content);
+#endif
