7c33e03526099f4f1fad63eca9d9152bae4c227b
Gabriel <gabrielvitorsales@gmail.com>
2021-12-28 21:54:58 -0300

    refactory: function split

--- src/ft_split.c
+++ src/ft_split.c
@@ -10,85 +10,102 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libft.h"
+#include <libft.h>
 
-static int	check(char **s, int num_p);
-static char	**alloc_str(char **str, char **p_tmp, int c);
-static int	n_pointers(char *s, char c);
+static void	destroy(char **result)
+{
+	char	**tmp;
+
+	tmp = result;
+	while (*tmp != NULL)
+	{
+		free(*tmp);
+		tmp++;
+	}
+}
+
+static int	fill_pointers(char **result, char *str, int n_pointers)
+{
+	char	**fill_pointers;
+	char	*tmp;
+
+	fill_pointers = result;
+	tmp = str;
+	while (n_pointers-- > 0)
+	{
+		while (*tmp == '\0')
+			tmp += 1;
+		*fill_pointers = ft_strdup(tmp);
+		if (*fill_pointers == NULL)
+			return (ERROR);
+		tmp += (ft_strlen(*fill_pointers) + 1);
+		fill_pointers += 1;
+	}
+	*fill_pointers = NULL;
+	return (SUCCESS);
+}
+
+static char	*change_c_to_null(const char *s, char c)
+{
+	char	*changed_str;
+	char	*holder;
+
+	changed_str = ft_strdup(s);
+	if (changed_str == NULL)
+		return (NULL);
+	holder = changed_str;
+	while (*holder)
+	{
+		while (*holder == c)
+			*holder = '\0';
+		holder += 1;
+	}
+	return (changed_str);
+}
+
+static int	number_of_pointers(const char *tmp, char c)
+{
+	int		n_pointers;
+
+	n_pointers = 0;
+	while (tmp != NULL && *tmp != 0)
+	{
+		if (*(tmp) != c && *(tmp) != 0)
+		{
+			n_pointers += 1;
+			tmp += (ft_strchr(tmp, c) - tmp);
+		}
+		if (tmp != NULL)
+			tmp += 1;
+	}
+	return (n_pointers);
+}
 
 char	**ft_split(const char *s, char c)
 {
-	char	*p_tmp;
-	char	**str;
-	char	set[2];
-	char	*pp;
+	char	**result;
+	char	*tmp;
+	char	*changed_str;
+	int		n_pointers;
 
-	if (!s)
+	tmp = (char *)s;
+	while (*tmp == c)
+		tmp += 1;
+	n_pointers = number_of_pointers(tmp, c);
+	result = (char **)malloc(sizeof(char *) * (n_pointers + 1));
+	if (result == NULL)
 		return (NULL);
-	set[0] = c;
-	set[1] = 0;
-	p_tmp = ft_strtrim(s, set);
-	if (!p_tmp)
-		return (NULL);
-	str = (char **)ft_calloc(sizeof(char *), n_pointers(p_tmp, c) + 1);
-	pp = p_tmp;
-	while (*p_tmp)
+	changed_str = change_c_to_null(tmp, c);
+	if (changed_str == NULL)
 	{
-		str = alloc_str(str, &p_tmp, c);
-		while (*p_tmp == c && c != 0)
-			p_tmp++;
+		destroy(result);
+		return (free(result), NULL);
 	}
-	free(pp);
-	return (str);
-}
-
-static char	**alloc_str(char **str, char **p_tmp, int c)
-{
-	int			len;
-	static int	num_p;
-
-	len = 0;
-	while ((p_tmp[0][len]) != c && p_tmp[0][len] != '\0')
-		len++;
-	if (!str[0])
-		num_p = 0;
-	*(str + (num_p)) = ft_substr(p_tmp[0], 0, len);
-	if (check(str, num_p))
-		return (NULL);
-	num_p++;
-	(p_tmp[0]) += len;
-	return (str);
-}
-
-static int	n_pointers(char *s, char c)
-{
-	int	counter;
-	int	pointers_to_alloc;
-
-	pointers_to_alloc = 0;
-	counter = 0;
-	while (s[counter])
+	if (fill_pointers(result, changed_str, n_pointers) == ERROR)
 	{
-		pointers_to_alloc++;
-		while (s[counter] != c && s[counter] != '\0')
-			counter++;
-		while (s[counter] == c && c != 0)
-			counter++;
+		destroy(result);
+		free(changed_str);
+		return (free(result), NULL);
 	}
-	return (pointers_to_alloc);
-}
-
-static int	check(char **s, int num_p)
-{
-	int	i;
-
-	i = 0;
-	if (!*(s + num_p))
-	{
-		while (i < num_p)
-			free(s[i++]);
-		s = NULL;
-		return (1);
-	}
-	return (0);
+	return (free(changed_str), result);
 }
